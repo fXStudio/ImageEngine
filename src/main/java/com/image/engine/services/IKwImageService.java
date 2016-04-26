cddff7efc68c0b1cade27e0745035cd2f884f1ef
Ajaxfan <ajax_2003@163.com>
2016-04-26 14:54:28 +0800

    modify

--- src/main/java/com/image/engine/services/IKwImageService.java
+++ src/main/java/com/image/engine/services/IKwImageService.java
@@ -6,9 +6,5 @@ import com.image.engine.models.KwImage;
  * @author FXStudio.Ajaxfan
  */
 public interface IKwImageService {
-	/**
-	 * @param id
-	 * @return
-	 */
 	public KwImage find(String id);
 }
