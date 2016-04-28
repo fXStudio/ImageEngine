061451841f9469c9dbced733f93213014b6d0850
Ajaxfan <ajax_2003@163.com>
2016-04-28 11:32:37 +0800

    框架变更

--- src/main/java/com/image/engine/mappers/KwImageMapper.java
+++ src/main/java/com/image/engine/mappers/KwImageMapper.java
@@ -2,7 +2,6 @@ package com.image.engine.mappers;
 
 import com.image.engine.models.KwImage;
 
-import tk.mybatis.mapper.common.base.select.SelectByPrimaryKeyMapper;
-
-public interface KwImageMapper extends SelectByPrimaryKeyMapper<KwImage> {
+public interface KwImageMapper {
+	public KwImage selectByPrimaryKey(String id);
 }
